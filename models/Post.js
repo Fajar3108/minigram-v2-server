166cad6fd33426cddce50111ef0c7f30166ab1ef
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-15 11:21:13 +0700

    Add: Post Model

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const PostSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.ObjectId,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    visibility: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        trim: true,
+        minlength: 5,
+        maxlength: 100,
+        required: false,
+    },
+    description: {
+        type: String,
+        text: true,
+        required: false,
+    },
+    video: {
+        type: String,
+        required: false,
+    },
+    images: [{
+        type: String,
+    }],
+    likes: [
+        mongoose.Schema({
+            user_id: {
+                type: mongoose.ObjectId,
+                required: true,
+            },
+        })
+    ],
+    comments: [
+        mongoose.Schema({
+            user_id: {
+                type: mongoose.ObjectId,
+                required: true,
+            },
+            body: {
+                type: String,
+                required: true,
+            },
+        }, {timestamps: true})
+    ],
+}, {timestamps: true});
+
+module.exports = mongoose.model('Post', PostSchema);
